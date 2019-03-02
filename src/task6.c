c1ac0823f47fd1ca1618662d16fb0422c5d36bc5
Ilya <ifed@tuta.io>
2019-03-02 10:44:21 +0300

    feat: add help section for circuit resistance calculation program
    - Add help section
    - Update binaries
    
    Part of 3.9

--- src/task6.c
+++ src/task6.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
+	if(argc == 1)
+		printf("Usage: task6.app [int int int int]\n\nArguments:\n    first section voltage\n    first section resistance\n    second section voltage\n    second second resistance\n");
 	if(argc != 5) {
 		printf("Wrong number of arguments.\n");
 		return -1;
